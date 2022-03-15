ba63ba9f343250cb1c6e170d27a2d8b636f81a5e
Amanda Esther Bonatti <estudosbonatti@gmail.com>
2022-03-15 15:54:50 -0300

    adicionando nav

--- react-zombi/src/App.js
+++ react-zombi/src/App.js
@@ -1,9 +1,11 @@
 import SiteHeader from './components/siteHeader/SiteHeader';
+import SiteNavBar from './components/SiteNavBar/SiteNavBar';
 
 function App() {
   return (
     <>
       <SiteHeader></SiteHeader>
+      <SiteNavBar></SiteNavBar>
     </>
   );
 }
--- react-zombi/src/components/siteHeader/SiteHeader.jsx
+++ react-zombi/src/components/siteHeader/SiteHeader.jsx
@@ -4,12 +4,12 @@ import { Button } from 'primereact/button';
 import './_siteHeader.css'
 import React, { Component } from 'react'
 
-class SiteHeader extends Component {
+class SiteHeader extends React.Component {
     render() {
         return (
             <div className='header-section'>
-                <div className='header-division-left'>
-                    <div className='header-division-left-content'>
+                <div className='header-division-left '>
+                    <div className='header-division-left-content header-size'>
                         <div className='left-content-phone'>
                             <i className="pi pi-phone"></i><span className="ml-1">+992 563 542</span>
                         </div>
@@ -19,15 +19,21 @@ class SiteHeader extends Component {
                     </div>
                 </div>
                 <div className='header-division-right'>
-                    <div className='header-division-right-content'>
+                    <div className='header-division-right-content header-size'>
                         <div className='right-content-user'>
                             <Button 
                                 label="Register"
                                 className="header-user-button"
                             />
+                            <Button 
+                                label="Log in"
+                                className="header-user-button"
+                            />
                         </div>
                         <div className='right-content-social_links'>
-                            <i className="pi pi-envelope"></i><span className="ml-1">info@mail.com</span>
+                            <i className="pi pi-discord"/>
+                            <i className="pi pi-twitter"/>
+                            <i className="pi pi-github"/>
                         </div>
                     </div>
                 </div>
